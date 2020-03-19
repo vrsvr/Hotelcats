5a79961bfa91ea786e87565d8b90ea3998ca399b
vrsvr <varisovrifat@gmail.com>
2020-03-19 20:48:19 +0500

    Сверстал форму "Бронирования номеров" и карточку номера, также исправил адаптивную верстку.

--- js/app.js
+++ js/app.js
@@ -5,16 +5,38 @@ $(function () {
   let introH = $("#intro").innerHeight(),
   scrollOffset = $(window).scrollTop();
   let nav = $("#nav");
+  let navToggle = $("#navToggle");
 
   $("[data-scroll]").on("click", function(event) {
     event.preventDefault();
 
+    let $this = $(this);
     let elementId = $(this).data('scroll');
     let elementOffset = $(elementId).offset().top;
+
+    $("nav, a").removeClass("active");
+    $this.addClass("active");
+
+    $("#nav_toggle").removeClass("active");
 
     $("html, body").animate({
       scrollTop: elementOffset - 30
     }, 600)
+  });
+
+  $("#nav-toggle").on("click", function(event) {
+    event.preventDefault();
+
+    $(this).toggleClass("active");
+    $("nav").toggleClass("active");
+  });
+
+  $("[data-collapse]").on("click", function(event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let elementId = $(this).data('collapse');
+    $this.toggleClass('active');
   });
 
   let slider = $("#reviewsSlider");
@@ -24,7 +46,17 @@ $(function () {
     slidesToShow: 2,
     slidesToScroll: 2,
     dots: true,
-    arrows: true
+    arrows: true,
+    responsive: [{
+    breakpoint:575,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      centerMode: true,
+      arrows: false,
+      centerPadding: '-30px'
+    }
+  }]
   });
 
   let sliderr = $("#roomsSlider");
@@ -34,7 +66,17 @@ $(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
-    arrows: true
+    arrows: true,
+    responsive: [
+      {
+    breakpoint: 575,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      centerMode: true,
+      arrows: false
+    }
+  }]
   });
 
 });
